0a1735aa8599a1b93295554c40c2b0cd4b66eced
ogogorev <rednowall@mail.ru>
2017-04-04 13:41:13 +0600

    add material-ui, add Competitions bar, remove app bar

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,11 +3,12 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 
 import {fetchCompetitions} from '../actions/competitions';
-import AppBar from '../components/AppBar';
+import CompetitionsBar from '../components/CompetitionsBar';
 
 const propTypes = {
     dispatch: PropTypes.func.isRequired,
     competitions: PropTypes.array.isRequired,
+    competitionId: PropTypes.string
 };
 
 class App extends Component {
@@ -16,11 +17,15 @@ class App extends Component {
         dispatch(fetchCompetitions());
     }
 
+    handleClick = (value) => {
+        this.props.history.push('/competitions/' + value);
+    }
+
     render() {
-        const {competitions} = this.props;
+        const {competitions, competitionId} = this.props;
         return (
             <div className='app-container'>
-                <AppBar competitions={competitions} />
+                <CompetitionsBar competitions={competitions} competitionId={competitionId} handleClick={this.handleClick}/>
             </div>
         );
     }
@@ -28,8 +33,9 @@ class App extends Component {
 
 App.propTypes = propTypes;
 
-function mapStateToProps(state) {
+function mapStateToProps(state, ownProps) {
     return {
+        competitionId: ownProps.match.params.competitionId,
         competitions: state.competitions.competitions,
     }
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import {Router, Route} from 'react-router';
 import {createHashHistory} from 'history';
 import {routerMiddleware} from 'react-router-redux';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 import '../styles/main.scss';
 import App from './containers/App';
 import CompetitionContainer from './containers/CompetitionContainer';
@@ -20,12 +24,17 @@ const store = configureStore(middleware);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={history}>
-            <div>
-                <Route path='/' component={App} />
-                <Route path='/competitions/:competitionId' component={CompetitionContainer} />
-            </div>
-        </Router>
+        <MuiThemeProvider>
+            <Router history={history}>
+                <div>
+                    <Route path='/' component={App} />
+                    <Route path='/competitions/:competitionId' component={CompetitionContainer} />
+                </div>
+            </Router>
+        </MuiThemeProvider>
     </Provider>,
     document.getElementById('main')
 );
+
+
+// <Route path='/competitions/:competitionId' component={CompetitionContainer} />
